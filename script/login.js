76c1d50934a2b4602a1569ba446aa2758f4f2e1d
Rishad <rishadmahmud5599@gmail.com>
2026-04-04 09:19:43 +0600

    login funtionality done

--- script/login.js
+++ script/login.js
@@ -1,3 +1,18 @@
 document.getElementById("login-btn").addEventListener("click", function () {
-  
+  // get the mobile number
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+  console.log(contactNumber);
+  // get the pin
+  const inputPin = document.getElementById("input-pin");
+  const pin = inputPin.value;
+  console.log(pin);
+  // match mobile number and password
+  if (contactNumber == "01645522657" && pin == "1234") {
+    alert("Login success");
+    window.location.assign("/home.html")
+  } else {
+    alert("Login failed");
+    return;
+  }
 });
