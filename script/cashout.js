d3016096064b08eb24fc9e409edf4cf8f283c4b2
Rishad <rishadmahmud5599@gmail.com>
2026-04-08 17:37:14 +0600

    cashout functionality done

---
+++ script/cashout.js
@@ -0,0 +1,34 @@
+document.getElementById("cashout-btn").addEventListener("click", function () {
+  const cashoutNumberInput = document.getElementById("cashout-number");
+  const cashoutNumber = cashoutNumberInput.value;
+  console.log(cashoutNumber);
+  if (cashoutNumber.length != 11) {
+    alert("Invalid number");
+    return;
+  }
+
+  const cashoutAmountInput = document.getElementById("cashout-amount");
+  const cashoutAmount = cashoutAmountInput.value;
+  console.log(cashoutAmount);
+
+  const balanceElement = document.getElementById("balance");
+  const balance = balanceElement.innerText;
+  console.log(balance);
+
+  const newBalance = Number(balance) - Number(cashoutAmount);
+
+  if (newBalance < 0) {
+    alert("Invalid Input");
+    return;
+  }
+  const cashoutPinInput = document.getElementById("cashout-pin");
+  const cashoutPin = cashoutPinInput.value;
+  if (cashoutPin == "1234") {
+    alert("Cashout success");
+    balanceElement.innerText = newBalance;
+    console.log("new balance is", newBalance);
+  } else {
+    alert("Invalid Pin");
+    return;
+  }
+});
